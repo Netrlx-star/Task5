2365e60f57ee1d1bf593bfeeba48dffdcc2d5067
1 <1>
2025-12-12 16:10:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var v1 = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            var v2 = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            if (v1.Length > v2.Length)
+                Console.WriteLine("Longest vector: " + string.Join(" ", v1));
+            else if (v2.Length > v1.Length)
+                Console.WriteLine("Longest vector: " + string.Join(" ", v2));
+            else
+                Console.WriteLine("Vectors are of equal length.");
+
+        }
+    }
+}
